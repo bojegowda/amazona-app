2b9aeb27afb146f3b38054f28095d42527f553be
bojegowda671@gmail.com <bojegowda671@gmail.com>
2020-11-28 16:33:20 +0530

    created register screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,7 @@ import {
   productListReducer,
 } from "./reducers/productReducers";
 import {
+  userRegisterReducer,
   userSigninReducer
 } from "./reducers/userReducer";
 
@@ -31,7 +32,8 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
-  userSignin: userSigninReducer
+  userSignin: userSigninReducer,
+  userRegister: userRegisterReducer
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
